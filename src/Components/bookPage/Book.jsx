9fbd130f52bed67c3350a39dc2d753cd047e76de
Rihanul Islam <rihanthedev@gmail.com>
2023-05-29 22:50:17 +0600

    json problem

--- src/Components/bookPage/Book.jsx
+++ src/Components/bookPage/Book.jsx
@@ -3,6 +3,11 @@ import img2 from '../../assist/iamge1.jpg'
 import img3 from '../../assist/iamge3.jpg'
 
 const Book = () => {
+    fetch('db.json')
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data);
+    })
     return (
         <div>
             <div style={{ backgroundImage: `url("${img1}")` }} className="hero min-h-screen text-white bg-black-100">  
@@ -10,7 +15,7 @@ const Book = () => {
             <div>
                <h2 className='text-4xl font-bold text-center my-4'>সু উপদেশ গীত</h2>
                <div>
-                    
+                
                </div>
             </div>
         </div>
