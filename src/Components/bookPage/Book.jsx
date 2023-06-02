0e03dfcc347b47fe72037c1b073871b2d2a9fae4
Rihanul Islam <rihanthedev@gmail.com>
2023-06-03 00:35:39 +0600

    Version 1.0

--- src/Components/bookPage/Book.jsx
+++ src/Components/bookPage/Book.jsx
@@ -1,21 +1,26 @@
 import img1 from '../../assist/iamge2.jpg'
 import img2 from '../../assist/iamge1.jpg'
 import img3 from '../../assist/iamge3.jpg'
+import { useEffect, useState } from 'react';
+import BookCard from './BookCard';
 
 const Book = () => {
-    fetch('db.json')
-    .then(res=>res.json())
-    .then(data=>{
-        console.log(data);
-    })
+    const [poems,setPoems] = useState([])
+    useEffect(()=>{
+        fetch('db.json')
+        .then(res=>res.json())
+        .then(data=>setPoems(data))
+    },[])
     return (
         <div>
             <div style={{ backgroundImage: `url("${img1}")` }} className="hero min-h-screen text-white bg-black-100">  
             </div>
             <div>
                <h2 className='text-4xl font-bold text-center my-4'>সু উপদেশ গীত</h2>
-               <div>
-                
+               <div className='grid grid-cols-1 md:grid-cols-3 gap-5'>
+                {
+                    poems.map(poem=><BookCard poem={poem}></BookCard>)
+                }
                </div>
             </div>
         </div>
--- src/Components/bookPage/BookCard.jsx
+++ src/Components/bookPage/BookCard.jsx
@@ -1,13 +1,13 @@
 
-const BookCard = () => {
+const BookCard = ({poem}) => {
+    const {title,main} =poem;
+
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
             <div className="card-body">
-                <h2 className="card-title">Card title!</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
-                </div>
+                <h2 className="card-title">{title}</h2>
+                <p>{main}</p>
+               
             </div>
         </div>
     );
